11d3b6a7b8917eda576bc4f95b49afff7040e71b
narsimha1603 <narsimha.abhi1603@gmail.com>
2025-04-26 12:09:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    list.insertlast(10);
+    list.insertlast(20);
+    list.insertlast(30);
+    list.insertlast(40);
+    list.insert(100,2);
+    list.display();
+}
+}
